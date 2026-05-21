ba40982575a8d2a571de85dd702a962e53c60994
Zachary Rankin <rankin.zachary@gmail.com>
2026-05-20 17:06:10 -0700

    Update to latest and greatest
    - Update nginx, php, postgres, etc
    - No need for parsing urls anymore
    - Fix php-stan issues with some doc blocks

--- src/db_functions.php
+++ src/db_functions.php
@@ -10,32 +10,9 @@ class DbException extends Exception
 {
 }
 
-function pg_conn_str_from_url(string $url, int $conn_timeout = 10): string
-{
-    $host = parse_url($url, PHP_URL_HOST);
-    $port = parse_url($url, PHP_URL_PORT) ?: 5432;
-    $database = substr(parse_url($url, PHP_URL_PATH) ?: "", 1);
-    $user = parse_url($url, PHP_URL_USER);
-    $password = parse_url($url, PHP_URL_PASS);
-
-    // get additional options like application_name
-    parse_str(parse_url($url, PHP_URL_QUERY) ?: "", $options);
-    $options_str = implode(" ", array_map(function ($arg, $val) {
-        return "--{$arg}={$val}";
-    }, array_keys($options), $options));
-
-    $conn_str = "host={$host} port={$port} dbname={$database}"
-          . " user={$user} password={$password}"
-          . " connect_timeout={$conn_timeout}"
-          . " options='{$options_str}'";
-
-    return $conn_str;
-}
-
-
 function connect(string $url): Connection
 {
-    $conn = pg_connect(pg_conn_str_from_url($url), PGSQL_CONNECT_FORCE_NEW);
+    $conn = pg_connect($url, PGSQL_CONNECT_FORCE_NEW);
     if (!$conn) {
         throw new Exception("Failed to connect to database");
     }
@@ -43,6 +20,9 @@ function connect(string $url): Connection
     return $conn;
 }
 
+/**
+ * @param array<int|string, scalar|null> $params
+ */
 function query(Connection $conn, string $sql, array $params = []): Result
 {
     if (empty($params)) {
@@ -58,11 +38,15 @@ function query(Connection $conn, string $sql, array $params = []): Result
     return $result;
 }
 
+/**
+ * @return callable(): Connection
+ */
 function db(string $db_url): callable
 {
-    static $db;
-    return function () use ($db, $db_url): Connection {
-        if ($db) {
+    return function () use ($db_url): Connection {
+        /** @var Connection|null $db */
+        static $db = null;
+        if ($db !== null) {
             return $db;
         }
 
@@ -70,15 +54,4 @@ function db(string $db_url): callable
 
         return $db;
     };
-}
-
-function read_rows_assoc(Result $res, callable $cb = null): iterable
-{
-    while ($row = pg_fetch_assoc($res)) {
-        if ($cb) {
-            yield $cb($row);
-        } else {
-            yield $row;
-        }
-    }
 }
